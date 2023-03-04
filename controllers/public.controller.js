173ee995a05c0f20d0800957edd3dad7c07134ee
unknown <pablojamiro2008@hotmail.com>
2023-03-03 21:20:01 -0500

    fix

--- controllers/public.controller.js
+++ controllers/public.controller.js
@@ -1,6 +1,6 @@
 const homepage = (req, res) => {
     res.render("index", {
-        page: "BIENVENIDO!",
+        page: "¡BIENVENIDO!",
         description:
             "En esta página podrás subir las imágenes para las promociones desplegadas en PIANO, recuerda subir imágenes con responsabilidad :)",
     });
