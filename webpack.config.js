a7e918d14a996295002c256e7da50d8c6ab897fd
unknown <pablojamiro2008@hotmail.com>
2023-03-04 10:50:09 -0500

    feat

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 import path from "path";
 
 export default {
-  mode: "development",
-  entry: {
-    preview: "./src/js/preview.js",
-    // agregarImagen: "./src/js/agregarImagen.js",
-  },
-  output: {
-    filename: "[name].js",
-    path: path.resolve("public/js"),
-  },
+    mode: "production",
+    entry: {
+        preview: "./src/js/preview.js",
+        // agregarImagen: "./src/js/agregarImagen.js",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve("public/js"),
+    },
 };
